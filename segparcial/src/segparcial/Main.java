3de9f0bea3ad882c0e4b69d22606d8d897dafbfe
John Castillo <john.gnu@gmail.com>
2022-10-28 13:10:47 -0400

    p4

--- segparcial/src/segparcial/Main.java
+++ segparcial/src/segparcial/Main.java
@@ -20,11 +20,17 @@ public class Main {
         // llenar categorias
         lc.adiFinal("c1", "Artes musicales", lks1);
         lc.adiFinal("c2", "Artes escenicas", lks1);
-        
+
         lc.mostrar();
 
+        // Lista Artistas
+        ArrayList<Artista> la = new ArrayList<>();
+        Artista a1 = new Artista("a1", "Bony Lovy", "112233", "28/10/2022", "musica", "c1", "s1", "individual");
+
+        la.add(a1);
+
         // Departamentos
-        NodoD d1 = new NodoD("Chuquizaca", new ArrayList<>());
+        NodoD d1 = new NodoD("Chuquizaca", la);
         NodoD d2 = new NodoD("La Paz", new ArrayList<>());
         NodoD d3 = new NodoD("Cochabamba", new ArrayList<>());
         NodoD d4 = new NodoD("Oruro", new ArrayList<>());
@@ -35,26 +41,17 @@ public class Main {
         NodoD d9 = new NodoD("Pando", new ArrayList<>());
         LDobleD ld = new LDobleD();
         ld.adifinal(d1);
-        ld.adifinal(d2);
-        ld.adifinal(d3);
-        ld.adifinal(d4);
-        ld.adifinal(d5);
-        ld.adifinal(d6);
-        ld.adifinal(d7);
-        ld.adifinal(d8);
-        ld.adifinal(d9);
+        //ld.adifinal(d2);
+        //ld.adifinal(d3);
+        //ld.adifinal(d4);
+        //ld.adifinal(d5);
+        //ld.adifinal(d6);
+        //ld.adifinal(d7);
+        //ld.adifinal(d8);
+        //ld.adifinal(d9);
 
         ld.mostrar();
 
-        // ategorias
-        // Artistas
-        Artista a1 = new Artista("a1", "Bony Lovy", "112233", "28/10/2022", "musica", "c1", "s1", "individual");
-
-        a1.mostrar();
-
-        // Array Artistas
-        ArrayList<Artista> la = new ArrayList<>();
-        la.add(a1);
     }
 
 }
