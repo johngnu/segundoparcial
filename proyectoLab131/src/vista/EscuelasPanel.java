e17ab4411820dce166f2f96238896a8e60ce613d
John Castillo <john.gnu@gmail.com>
2022-12-05 15:59:59 -0400

    hhhh

--- proyectoLab131/src/vista/EscuelasPanel.java
+++ proyectoLab131/src/vista/EscuelasPanel.java
@@ -22,7 +22,7 @@ import estructuras.NodoE;
 import estructuras.Participante;
 
 public class EscuelasPanel extends JPanel {
-
+    
     private JLabel titulo = new JLabel("<html><h1 style=\"background: #cb2525;color:white;padding: 20px\">ESCUELAS</h1></html>", JLabel.CENTER);
     private JTextField t1, t2;
     private JButton ejecutar, cancelar;
@@ -34,21 +34,21 @@ public class EscuelasPanel extends JPanel {
     private LSimpleM lm;
     private ColaParticipantes cp;
     private JLabel salida;
-
+    
     public EscuelasPanel(LSimpleM lm, LSimpleE le, ColaParticipantes cp) {
         this.lista = le;
         this.lm = lm;
         this.cp = cp;
         init();
     }
-
+    
     public void init() {
         CrearInterfaz();
         ejecutar();
         cancelar();
         CreateBox();
     }
-
+    
     public void cancelar() {
         cancelar = new JButton("CANCELAR");
         cancelar.addActionListener(new ActionListener() {
@@ -72,7 +72,7 @@ public class EscuelasPanel extends JPanel {
             }
         });
     }
-
+    
     public void CrearInterfaz() {
         setLayout(new BorderLayout());
         titulo.setAlignmentY(CENTER_ALIGNMENT);
@@ -80,15 +80,15 @@ public class EscuelasPanel extends JPanel {
         titulo.setBackground(Color.decode("#cb2525"));
         titulo.setOpaque(true);
         titulo.setPreferredSize(new Dimension(200, 60));
-
+        
         salida = new JLabel();
         salida.setBorder(new LineBorder(Color.white));
-
+        
         Object data[][] = llenarTabla();
         String[] columnNames = {"ID", "NOMBRE", "TELF.", "DIRECCION"};
         model = new DefaultTableModel(data, columnNames);
         tabla = new JTable(model);
-
+        
         tablaSp = new JScrollPane(tabla);
         //tablaSp.setPreferredSize(new Dimension(400, 120));
 
@@ -97,47 +97,47 @@ public class EscuelasPanel extends JPanel {
         t1.setMaximumSize(t1.getPreferredSize());
         t2.setMaximumSize(t2.getPreferredSize());
     }
-
+    
     public void CreateBox() {
         Box h1 = Box.createHorizontalBox();
         h1.add(titulo);
-
+        
         Box cajaIzq = Box.createVerticalBox();
         cajaIzq.add(new JLabel("PROBLEMA 3"));
-
+        
         Box enbox = Box.createHorizontalBox();
         enbox.add(new JLabel("<html>Mostrar al Participante con mayor edad inscrito en la escuela X</html>"));
         cajaIzq.add(enbox);
-
+        
         Box caja1 = Box.createHorizontalBox();
         caja1.add(new JLabel("ID:"));
         caja1.add(Box.createHorizontalStrut(10));
         caja1.add(t1);
         cajaIzq.add(caja1);
-
+        
         Box outbox = Box.createHorizontalBox();
         outbox.add(salida);
         cajaIzq.add(outbox);
-
+        
         Box caja5 = Box.createHorizontalBox();
         //caja5.add(cancelar);
         caja5.add(Box.createHorizontalStrut(10));
         caja5.add(ejecutar);
         cajaIzq.add(caja5);
-
+        
         Box cajaDer = Box.createVerticalBox();
         cajaDer.add(tablaSp);
-
+        
         add(h1, BorderLayout.NORTH);
         add(cajaIzq, BorderLayout.CENTER);
         add(Box.createRigidArea(new Dimension(5, 0)), BorderLayout.WEST);
         add(cajaDer, BorderLayout.EAST);
     }
-
+    
     public Object[][] llenarTabla() {
         int x = lista.nroNodos();
         Object[][] v = new Object[x][4];
-
+        
         NodoE r = lista.getP();
         int i = 0;
         while (r != null) {
@@ -164,20 +164,21 @@ public class EscuelasPanel extends JPanel {
                     JOptionPane.showMessageDialog(null, "RELLENE DATOS");
                     return;
                 }
-
+                
                 boolean sw = false;
                 int may = 0;
-
+                
                 NodoE r = lista.getP();
                 while (r != null) {
                     if (r.getIdEscuela().equals(t1.getText())) {
                         Participante elem;
-
                         ColaParticipantes aux = new ColaParticipantes();
-                        while (!lista.esvacia()) {
+                        while (!cp.esvacia()) {
                             elem = cp.eliminar();
-                            if (elem.getEdad() > may) {
-                                may = elem.getEdad();
+                            if (elem.getIdEscuela().equals(r.getIdEscuela())) {
+                                if (elem.getEdad() > may) {
+                                    may = elem.getEdad();
+                                }
                             }
                             aux.adicionar(elem);
                         }
@@ -185,9 +186,9 @@ public class EscuelasPanel extends JPanel {
                     }
                     r = r.getSig();
                 }
-
+                
                 salida.setText("El mayor tiene :" + may + " anios");
-
+                
             }
         });
     }
